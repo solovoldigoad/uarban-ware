9731e4be0f55ea8e2bdfa026c5bf18e98b7143f4
Kush Chauhan <kushchauhan2004@gmail.com>
2026-03-05 17:13:16 +0530

    deploying and correctino the build

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export async function middleware(req: any) {
+export async function proxy(req: any) {
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
